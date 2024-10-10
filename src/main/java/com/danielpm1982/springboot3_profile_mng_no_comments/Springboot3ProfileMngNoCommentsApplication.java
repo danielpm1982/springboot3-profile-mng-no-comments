b677a71c4eb609c8ae1e871a032f21fc3314ed6e
danielpm1982 <danielpm1982@gmail.com>
2024-10-10 04:03:51 -0300

    Add base project
    Remove .idea directory
    
    .

---
+++ src/main/java/com/danielpm1982/springboot3_profile_mng_no_comments/Springboot3ProfileMngNoCommentsApplication.java
@@ -0,0 +1,13 @@
+package com.danielpm1982.springboot3_profile_mng_no_comments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3ProfileMngNoCommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3ProfileMngNoCommentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielpm1982/springboot3_profile_mng_no_comments/Springboot3ProfileMngNoCommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielpm1982.springboot3_profile_mng_no_comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ProfileMngNoCommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
